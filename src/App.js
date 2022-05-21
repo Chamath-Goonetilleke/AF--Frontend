d1ff10b24035c3bb0d7dac8701d640e08e68ae89
Kavindu Madushan <kavindu@tsdclanka.com>
2022-05-22 02:37:54 +0530

    frontend adding new testes

--- src/App.js
+++ src/App.js
@@ -12,18 +12,20 @@ import ReportSubmission from "./components/IT20122614/ReportSubmission";
 import ProposalSubmit from "./components/IT20122614/ProposalSubmit";
 import FinalThesis from "./components/IT20122614/FinalThesis";
 
-
 function App() {
   return (
     <div>
       <Routes>
-        
-        <Route path="/" exact component={StudentProfile} />
+        <Route path="/student-profile" exact component={StudentProfile} />
         <Route path="/register-topic" exact component={RegisterTopic} />
         <Route path="/register-group" exact component={RegisterGroup} />
         <Route path="/supervisor" exact component={RequestSupervisor} />
         <Route path="/cosupervisor" exact component={RequestCoSupervisor} />
-        <Route path="/submit-presentation" exact component={SubmitPresentation} />
+        <Route
+          path="/submit-presentation"
+          exact
+          component={SubmitPresentation}
+        />
         <Route path="/submit-report" exact component={ReportSubmission} />
         <Route path="/submit-proposal" exact component={ProposalSubmit} />
         <Route path="/submit-thesis" exact component={FinalThesis} />
