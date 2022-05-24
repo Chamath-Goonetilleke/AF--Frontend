299c3f700046c61867611c7a0741e73d493905e5
Vihanga Pramudith <88036867+VihangaPramudith@users.noreply.github.com>
2022-05-25 02:36:07 +0530

    implement navbar for navigation

---
+++ src/components/IT20122096/IT20192082/NavBar.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import "bootstrap";
+
+export default class Navbar extends Component {
+
+    render() {
+        return(
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+            <li className="nav-item active">
+            <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="/">Topics</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="/researchgroups">ResearchGroups</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="/report">Report</a>
+            </li>
+            </ul>
+            </div>
+            </nav>
+        )
+    }
+}
