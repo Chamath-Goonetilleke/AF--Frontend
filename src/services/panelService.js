2f30a4b94873c59e6be976d6d82c445f5051b1fe
Vihanga Pramudith <88036867+VihangaPramudith@users.noreply.github.com>
2022-05-25 01:59:44 +0530

    initialize panel service for connect backend route

---
+++ src/services/panelService.js
@@ -0,0 +1,36 @@
+import config from "../config.json";
+import service from "./httpService";
+
+const url = config.API;
+
+export async function topics() {
+    return await service.get(url +"/topics");
+}
+
+export async function getTopic(id) {
+    return await service.get(url +`/topic/${id}`);
+}
+
+export async function updateTopic(id, data) {
+    return await service.put(url + `/topic/update/${id}`, data);
+}
+
+export async function researchgroups() {
+    return await service.get(url +`/researchgroups`);
+}
+
+export async function getResearchGroup(id) {
+    return await service.get(url +`/researchgroup/${id}`);
+}
+
+export async function criteriass() {
+    return await service.get(url +`/criterias`);
+}
+
+export async function saveCriterias(data) {
+    return await service.post(url +`/criterias/save`,data);
+}
+
+export async function generateReport() {
+    return await service.get(url +`/report`);
+}
